a4f9739daf8fffe3365aa6a107c887346865c5f5
Qijin Chau <65829879+Qijin-Chau@users.noreply.github.com>
2020-09-29 11:12:57 -0400

    Add files via upload

---
+++ Scripts/navbar.js
@@ -0,0 +1,9 @@
+function showNav() {
+	var x = document.getElementById("myNav");
+	
+	if (x.className === "nav") {
+		x.className += " responsive";
+	} else {
+		x.className = "nav";
+	}
+}
---
+++ Scripts/showdesc.js
@@ -0,0 +1,44 @@
+function showDescription(descID) {
+	var myVar = document.getElementById(descID);
+	
+	if (myVar.style.display === "none") {
+		myVar.style.display = "block";
+		myVar.style.margin = "5px 0 25px 10px";
+		myVar.style.width = "90%";
+		myVar.style.float = "none";
+	} else {
+		myVar.style.display = "none";
+	}
+}
+
+function showAllFreshman() {
+	myIDS = ["Math32", "Math34", "Phy11", "Chem01", "Chn01", "Chn02", "Comp11", "EN01", "ED119"];
+	
+	for (i = 0; i < myIDS.length; i++) {
+		myVar = document.getElementById(myIDS[i]);
+		if (myVar.style.display === "none") {
+			myVar.style.display = "block";
+			myVar.style.margin = "5px 0 25px 10px";
+			myVar.style.width = "90%";
+			myVar.style.float = "none";
+		} else {
+			myVar.style.display = "none";
+		}
+	}
+}
+
+function showAllSophomore() {
+	myIDS = ["Comp61", "Math70", "ES11", "ES03", "ES04", "Comp20", "Comp15", "Comp160", "Chn03", "EXP53"];
+	
+	for (i = 0; i < myIDS.length; i++) {
+		myVar = document.getElementById(myIDS[i]);
+		if (myVar.style.display === "none") {
+			myVar.style.display = "block";
+			myVar.style.margin = "5px 0 25px 10px";
+			myVar.style.width = "90%";
+			myVar.style.float = "none";
+		} else {
+			myVar.style.display = "none";
+		}
+	}
+}
